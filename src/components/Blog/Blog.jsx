b024e1724109637d3d5c9b32e58424e7f01d4981
abdur rahman <rsaj53@gmail.com>
2025-04-05 13:06:49 +0200

    blog created

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,49 @@
 
 
-const Blog = () => {
-   
+const Blog = ({blog}) => {
+   const{author,author_img,cover,hashtags,posted_date,reading_time,title}=blog
     return (
-        <div>
+        <div className="mb-4 border-x-4 p-2 "> 
+            <img className="mb-4 w-full" src={cover} alt="" />
+            <div className="flex justify-between m-6">
+                <div className="flex">
+                    <div className="m-2 mt-4">
+                    <img className="w-12" src={author_img} alt="" />
+
+                    </div>
+                    <div className="m-2">
+                          
+                    <h5>
+                        {
+                            author
+                        
+                        }
+
+                    </h5>
+                    <p>
+                        {posted_date}
+
+                    </p>
+
+                    </div>
+                 
+                </div>
+                <div>
+                    <span>{reading_time} Min Read</span>
+
+                </div>
+            </div>
+            <h3 className="mx-4 text-4xl my-2">{title}</h3>
+            <p className="mx-6 my-1">
+            {
+                hashtags.map(hash=><span>
+                   #{hash}
+                </span>)
+            }
+            </p>
+            <button className="underline mx-6">
+                Mark as read
+            </button>
             
         </div>
     );
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import  { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import Blog from '../Blog/Blog';
 
 const Blogs = props => {
     const [blogs,setBlogs]=useState([])
     useEffect(()=>{
         fetch('blogs.json')
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>setBlogs(data))
 
     },[])
     return (
-        <div>
+        <div className='w-2/3 m-4'>
+            {
+                blogs.map(blog=><Blog blog={blog}></Blog>)
+            }
             
         </div>
     );
